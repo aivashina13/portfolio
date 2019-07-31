3aa6c752a22f54c78bf546986ab31d7a3f4d52b5
aivashina13 <53527481+aivashina13@users.noreply.github.com>
2019-07-31 10:03:32 -0400

    Added files via upload

---
+++ photos.js
@@ -0,0 +1,127 @@
+function ukraineinc() {
+  document.getElementById("ukraine").style.width="35%";
+  document.getElementById("ukraine").style.height="35%";
+  document.getElementById("ukraine").setAttribute = ("align", "center");
+  document.getElementById("architecture").style.visibility = "hidden";
+  document.getElementById("nyc").style.visibility = "hidden";
+  document.getElementById("bus").style.visibility = "hidden";
+  document.getElementById("gardens").style.visibility = "hidden";
+  document.getElementById("prague").style.visibility = "hidden";
+}
+
+function ukrainedec() {
+  document.getElementById("ukraine").style.width="20%";
+  document.getElementById("ukraine").style.height="20%";
+  document.getElementById("architecture").style.visibility = "visible";
+  document.getElementById("nyc").style.visibility = "visible";
+  document.getElementById("bus").style.visibility = "visible";
+  document.getElementById("gardens").style.visibility = "visible";
+  document.getElementById("prague").style.visibility = "visible";
+}
+
+
+
+function archinc() {
+  document.getElementById("architecture").style.width="50%";
+  document.getElementById("architecture").style.height="50%";
+  document.getElementById("ukraine").style.visibility = "hidden";
+  document.getElementById("nyc").style.visibility = "hidden";
+  document.getElementById("bus").style.visibility = "hidden";
+  document.getElementById("gardens").style.visibility = "hidden";
+  document.getElementById("prague").style.visibility = "hidden";
+
+}
+function archdec() {
+  document.getElementById("architecture").style.width="35%";
+  document.getElementById("architecture").style.height="35%";
+  document.getElementById("ukraine").style.visibility = "visible";
+  document.getElementById("nyc").style.visibility = "visible";
+  document.getElementById("bus").style.visibility = "visible";
+  document.getElementById("gardens").style.visibility = "visible";
+  document.getElementById("prague").style.visibility = "visible";
+}
+
+
+
+function nycinc() {
+  document.getElementById("nyc").style.width="50%";
+  document.getElementById("nyc").style.height="50%";
+  document.getElementById("architecture").style.visibility = "hidden";
+  document.getElementById("ukraine").style.visibility = "hidden";
+  document.getElementById("bus").style.visibility = "hidden";
+  document.getElementById("gardens").style.visibility = "hidden";
+  document.getElementById("prague").style.visibility = "hidden";
+
+}
+function nycdec() {
+  document.getElementById("nyc").style.width="20%";
+  document.getElementById("nyc").style.height="20%";
+  document.getElementById("architecture").style.visibility = "visible";
+  document.getElementById("ukraine").style.visibility = "visible";
+  document.getElementById("bus").style.visibility = "visible";
+  document.getElementById("gardens").style.visibility = "visible";
+  document.getElementById("prague").style.visibility = "visible";
+}
+
+
+function pragueinc() {
+  document.getElementById("prague").style.width="50%";
+  document.getElementById("prague").style.height="50%";
+  document.getElementById("architecture").style.visibility = "hidden";
+  document.getElementById("ukraine").style.visibility = "hidden";
+  document.getElementById("bus").style.visibility = "hidden";
+  document.getElementById("gardens").style.visibility = "hidden";
+  document.getElementById("nyc").style.visibility = "hidden";
+
+}
+function praguedec() {
+  document.getElementById("prague").style.width="30%";
+  document.getElementById("prague").style.height="30%";
+  document.getElementById("architecture").style.visibility = "visible";
+  document.getElementById("ukraine").style.visibility = "visible";
+  document.getElementById("bus").style.visibility = "visible";
+  document.getElementById("gardens").style.visibility = "visible";
+  document.getElementById("nyc").style.visibility = "visible";
+}
+
+function businc() {
+  document.getElementById("bus").style.width="50%";
+  document.getElementById("bus").style.height="50%";
+  document.getElementById("architecture").style.visibility = "hidden";
+  document.getElementById("ukraine").style.visibility = "hidden";
+  document.getElementById("nyc").style.visibility = "hidden";
+  document.getElementById("gardens").style.visibility = "hidden";
+  document.getElementById("prague").style.visibility = "hidden";
+
+}
+function busdec() {
+  document.getElementById("bus").style.width="30%";
+  document.getElementById("bus").style.height="30%";
+  document.getElementById("architecture").style.visibility = "visible";
+  document.getElementById("ukraine").style.visibility = "visible";
+  document.getElementById("nyc").style.visibility = "visible";
+  document.getElementById("gardens").style.visibility = "visible";
+  document.getElementById("prague").style.visibility = "visible";
+}
+
+
+
+function gardensinc() {
+  document.getElementById("gardens").style.width="50%";
+  document.getElementById("gardens").style.height="50%";
+  document.getElementById("architecture").style.visibility = "hidden";
+  document.getElementById("ukraine").style.visibility = "hidden";
+  document.getElementById("bus").style.visibility = "hidden";
+  document.getElementById("nyc").style.visibility = "hidden";
+  document.getElementById("prague").style.visibility = "hidden";
+
+}
+function gardensdec() {
+  document.getElementById("gardens").style.width="20%";
+  document.getElementById("gardens").style.height="20%";
+  document.getElementById("architecture").style.visibility = "visible";
+  document.getElementById("ukraine").style.visibility = "visible";
+  document.getElementById("bus").style.visibility = "visible";
+  document.getElementById("nyc").style.visibility = "visible";
+  document.getElementById("prague").style.visibility = "visible";
+}
